e58a9e0ce5e7af30185c1c5f735085d974544f44
Arun Venkatesan <arun.venkatesan@transunion.com>
2023-04-23 05:49:17 +0530

    Added job

--- my-website/build-image.sh
+++ my-website/build-image.sh
@@ -1,10 +1,13 @@
 docker build -t my-website:0.0.1 .
 
 # run docker image in container
-# docker run --rm --name my-website -d -p8080:80 my-website:0.0.1
+docker run --rm --name my-website -d -p8080:80 my-website:0.0.1
 
 # check the webiste is running
-# curl localhost:8080
+curl localhost:8080
 
 # stop the website in docker
-# docker stop my-website
+docker stop my-website
+
+# archive image to a file
+docker save -o /home/arvenka/my-website_0.0.1.tar my-website:0.0.1
