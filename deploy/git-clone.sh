6562c4847e350056ff04cb5d6171c547d6b66287
Arun Venkatesan <arun.venkatesan@transunion.com>
2023-04-25 15:20:39 +0530

    Added cron job

---
+++ deploy/git-clone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/arunvambur/kubernetes-experiments.git
+cd kubernetes-experiments/deploy
+chmod u+x k8s-cluster-common-deploy.sh k8s-cluster-cp-deploy.sh
+./k8s-cluster-common-deploy.sh
